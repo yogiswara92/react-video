69c6276d46d16c92d6dc29d0a0bd5c0d8543e672
yogiswara92 <yogiswaragheartha@gmail.com>
2022-04-18 21:17:27 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4mj9wb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4mj9wb)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+import {Player} from 'video-react';
+import "./player.css";
+
+export default function App() {
+  // referensi:https://video-react.js.org/
+  
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+
+      <Player
+        playsInline
+        poster="/assets/poster.png"
+        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      />
+    </div>
+    
+      
+  );
+}
